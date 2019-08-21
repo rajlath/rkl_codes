71d412a46b488e500416c1f9d0ae1ed5ee9ca8a9
raj.rinfo <raj.lath@gmail.com>
2019-08-21 15:15:25 +0530

    updated

--- CodeForces/CFR_581_2_1204/bowwow_and_timetable_A.kt
+++ CodeForces/CFR_581_2_1204/bowwow_and_timetable_A.kt
@@ -1,6 +1,6 @@
 fun main()
 {
-    val (given, left, rite) = readLine().split(' ').map{ it.toInt()}
+    val (given, left, rite) = readLine()!!.split(' ').map{ it.toInt()}
     val mins = (1 shl left) + (given - left - 1)
     val maxs = (1 shl rite) - 1 + (1 shl rite - 1) * (given - rite)
     println("$mins $maxs")
